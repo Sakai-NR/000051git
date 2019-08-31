2c4806dfae902398e61e2f6bb73a581b730d5233
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-08-31 23:24:51 +0900

    000051

---
+++ 000051/000051/ScondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ScondViewController.swift
+//  000051
+//
+//  Created by 酒井典昭 on 2019/08/31.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class ScondViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension ScondViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension ScondViewController {
+    
+}
+
+// MARK: - method
+extension ScondViewController {
+    
+}
+
